449f066d8580d0840d8986d84e3148e982ae0288
Sven Wappler <info@wappler.systems>
2023-07-20 00:17:41 +0200

    fix logout

--- Classes/Hooks/LogoutHook.php
+++ Classes/Hooks/LogoutHook.php
@@ -3,6 +3,7 @@
 namespace WapplerSystems\MultisiteBelogin\Hooks;
 
 use TYPO3\CMS\Core\Authentication\AbstractUserAuthentication;
+use TYPO3\CMS\Core\Session\Backend\HashableSessionBackendInterface;
 use WapplerSystems\MultisiteBelogin\Session\UserSessionManager;
 
 class LogoutHook
@@ -14,8 +15,16 @@ class LogoutHook
         $sessionBackend = $userSessionManager->getSessionBackend();
         $userSessions = $sessionBackend->getAll();
 
+        if ($pObj->user === null) {
+            return;
+        }
+        $sessionIdentifier = $pObj->getSession()->getIdentifier();
+        if ($sessionBackend instanceof HashableSessionBackendInterface) {
+            $sessionIdentifier = $sessionBackend->hash($sessionIdentifier);
+        }
+
         foreach ($userSessions as $userSession) {
-            if ($userSession['ses_userid'] !== $pObj->getSession()->getIdentifier() && $userSession['ses_userid'] === $pObj->user['uid']) {
+            if ($userSession['ses_id'] !== $sessionIdentifier && $userSession['ses_userid'] === $pObj->user['uid']) {
                 $sessionBackend->remove($userSession['ses_id']);
             }
         }
