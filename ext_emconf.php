9616ee7e3a6e37e824d04a8b1d6beeff6f4107d6
Sven Wappler <info@wappler.systems>
2023-08-19 23:10:14 +0200

    update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['multisite_belogin'] = [
     'title' => 'Multisite Backend Login',
-    'description' => '',
+    'description' => 'Cross site/domain backend login for TYPO3. It allows backend users to work in the frontend across domains.',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3@wappler.systems',
     'category' => 'be',
