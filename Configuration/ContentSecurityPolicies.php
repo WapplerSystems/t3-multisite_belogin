614587443e699a52a61d20d1466be489a867947d
Sven Wappler <wappler@wappler.systems>
2025-11-21 15:50:24 +0100

    add backend csps

---
+++ Configuration/ContentSecurityPolicies.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Mutation;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationCollection;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationMode;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Scope;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue;
+use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Type\Map;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+$currentHost = $_SERVER['HTTP_HOST'];
+$siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+$sites = $siteFinder->getAllSites();
+
+$mutations = [];
+
+foreach ($sites as $site) {
+    $host = parse_url($site->getBase()->__toString(), PHP_URL_HOST);
+    if ($host && $host !== $currentHost) {
+        $mutations[] = new Mutation(
+            MutationMode::Extend,
+            Directive::ConnectSrc,
+            new UriValue($site->getBase()->__toString()),
+        );
+    }
+}
+$collection = new MutationCollection(...$mutations);
+
+return Map::fromEntries([
+    // Provide declarations for the backend
+    Scope::backend(),
+    $collection
+    ]
+);
