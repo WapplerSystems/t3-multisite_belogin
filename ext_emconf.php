a9d191e0a54163e70a3441d9470c78e983a3fd16
Sven Wappler <wappler@wappler.systems>
2026-02-22 16:25:46 +0100

    update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF['multisite_belogin'] = [
     'author_company' => 'WapplerSystems',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '12.1.0',
+    'version' => '14.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.8-12.4.99',
+            'typo3' => '14.0.0-14.4.99',
         ],
     ],
 ];
